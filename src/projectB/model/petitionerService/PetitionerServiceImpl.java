6fd1f4ac5dc5412c795e4ff4e9691f82105d736e
baek <asx14@naver.com>
2021-01-14 23:36:45 +0900

    feat : 이메일 인증 확인 추가 & 인증 성공 시 상태 값 변경

--- src/projectB/model/petitionerService/PetitionerServiceImpl.java
+++ src/projectB/model/petitionerService/PetitionerServiceImpl.java
@@ -1,5 +1,7 @@
 package projectB.model.petitionerService;
 
+import java.util.HashMap;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,5 +22,24 @@ public class PetitionerServiceImpl implements PetitionerService {
 	@Override
 	public int confirmId(String id) {
 		return connection.selectOne("petitioner.confirmId", id);
+	}
+	
+	@Override
+	public int confirmAuthKey(PetitionerDTO dto) {
+		return connection.selectOne("petitioner.confirmAuthKey", dto);
+	}
+	
+	@Override
+	public PetitionerDTO getPetitionerByEmailAndAuthKey(String email, String authKey) {
+		HashMap<String, String> dataMap = new HashMap<>();
+		dataMap.put("email", email);
+		dataMap.put("authKey", authKey);
+		return connection.selectOne("petitioner.getPetitionerByEmailAndAuthKey", dataMap);
+	}
+	
+	@Override
+	public void updatePetitionerState(String id) {
+		System.out.println("updatePetitionerState id - " + id);
+		connection.update("petitioner.updatePetitionerState", id);
 	}
 }
