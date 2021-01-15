5be3dbb452771341fc2a682cd0c570686264bbbb
Bobae <joybobae98@gmail.com>
2021-01-15 11:38:20 +0900

    chore : insert SQL수정 petitionBean병합 (#17)

--- src/projectB/model/petitionService/PetitionBean.java
+++ src/projectB/model/petitionService/PetitionBean.java
@@ -1,5 +1,6 @@
 package projectB.model.petitionService;
 
+import java.util.Collections;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import projectB.model.petition.PetitionDTO;
 
@@ -41,8 +43,122 @@ public class PetitionBean {
 	   return "wooch/uploadPro";
    }
 	
+   
+	//청원게시판 리스트
+	@RequestMapping("ing_list.aa")
+	public String ing_list(@RequestParam(defaultValue="1")int pageNum, Model model) throws Exception {
+		
+		System.out.println("viewTest");
+		int pageSize = 10;
+		int currentPage = pageNum;
+		int startRow = (currentPage - 1) * pageSize +1;
+		int endRow = currentPage * pageSize;
+		int count = 0;
+		int number = 0;
+		
+		List<PetitionDTO> articleList = null;
+		count = dao.getArticleCount();
+		if(count > 0) {
+			articleList = dao.getArticles(startRow, endRow);
+		} else {
+			articleList = Collections.emptyList();
+		}
+		number = count-(currentPage-1)*pageSize;
+		
+		 System.out.println(count + "//count");
+	     System.out.println(articleList.size() + "//size");
+		
+		model.addAttribute("currentPage", new Integer(currentPage));
+		model.addAttribute("startRow", new Integer(startRow));
+		model.addAttribute("endRow", new Integer(endRow));
+		model.addAttribute("count", new Integer(count));
+		model.addAttribute("pageSize", new Integer(pageSize));
+		model.addAttribute("number", new Integer(number));
+		model.addAttribute("articleList", articleList);
+		
+		return "board/ing_list";
+	}
 	
-	
+	//답변이 완료된 게시판 리스트
+	@RequestMapping("finish_list.aa")
+	public String test1() {
+		System.out.println("finish Test");
+		
+		System.out.println("ddd");
+		return "board/finish_list";
+	}
+
+	//기간이 만료된 청원 리스트
+	@RequestMapping("timeout_list.aa")
+	public String timeout_list(@RequestParam(defaultValue="1")int pageNum, Model model) throws Exception {
+		
+		System.out.println("timeout Test");
+		int pageSize = 10;
+		int currentPage = pageNum;
+		int startRow = (currentPage - 1) * pageSize +1;
+		int endRow = currentPage * pageSize;
+		int count = 0;
+		int number = 0;
+		int state= 3; // 기간이 만료된 청원 
+		
+		List<PetitionDTO> articleList = null;
+		count = dao.getArticleCountbyState();
+		if(count > 0) {
+			articleList = dao.getArtilclebyState(state,startRow, endRow);
+		} else {
+			articleList = Collections.emptyList();
+		}
+		number = count-(currentPage-1)*pageSize;
+		
+		 System.out.println(count + "//count");
+	     System.out.println(articleList.size() + "//size");
+		
+		model.addAttribute("currentPage", new Integer(currentPage));
+		model.addAttribute("startRow", new Integer(startRow));
+		model.addAttribute("endRow", new Integer(endRow));
+		model.addAttribute("count", new Integer(count));
+		model.addAttribute("pageSize", new Integer(pageSize));
+		model.addAttribute("number", new Integer(number));
+		model.addAttribute("articleList", articleList);
+		
+		return "board/timeout_list";
+	}
+
+	//답변을 기다리는 리스트
+	@RequestMapping("waiting_list.aa")
+	public String test3(@RequestParam(defaultValue="1")int pageNum, Model model) throws Exception{
+		System.out.println("waiting Test");
+
+		int pageSize = 10;
+		int currentPage = pageNum;
+		int startRow = (currentPage - 1) * pageSize +1;
+		int endRow = currentPage * pageSize;
+		int count = 0;
+		int number = 0;
+		int state= 4; // 답변을 기다리는 청원
+		
+		List<PetitionDTO> articleList = null;
+		count = dao.getArticleCountbyState();
+		if(count > 0) {
+			articleList = dao.getArtilclebyState(state,startRow, endRow);
+		} else {
+			articleList = Collections.emptyList();
+		}
+		number = count-(currentPage-1)*pageSize;
+		
+		 System.out.println(count + "//count");
+	     System.out.println(articleList.size() + "//size");
+		
+		model.addAttribute("currentPage", new Integer(currentPage));
+		model.addAttribute("startRow", new Integer(startRow));
+		model.addAttribute("endRow", new Integer(endRow));
+		model.addAttribute("count", new Integer(count));
+		model.addAttribute("pageSize", new Integer(pageSize));
+		model.addAttribute("number", new Integer(number));
+		model.addAttribute("articleList", articleList);
+		
+		return "board/waiting_list";
+	}
 	
 	
 	
