e4ca6f50c524c0aba1a518df291169e58217eac0
baek <asx14@naver.com>
2021-01-18 14:40:45 +0900

    feat : 로그인 폼 / 기능 추가

---
+++ src/projectB/model/login/LoginUtils.java
@@ -0,0 +1,22 @@
+package projectB.model.login;
+
+import javax.servlet.http.HttpSession;
+import projectB.model.petitioner.PetitionerDTO;
+
+public class LoginUtils {
+	public static void setLogin(HttpSession session, PetitionerDTO dto, int loginReulst) {
+		if(dto == null || dto.getId() == null || loginReulst > 0) {
+			new IllegalAccessException("로그인 정보가 없습니다");			
+		}
+
+		session.setAttribute("memId", dto.getId());
+	}
+	
+	public static boolean isLogin(HttpSession session) {
+		return session.getAttribute("memId") != null;
+	}
+	
+	public static void logout(HttpSession session) {
+		session.invalidate();
+	}
+}
