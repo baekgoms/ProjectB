988e4d03bfbcb9557634e75792fc982782a443f2
baekgoms <asx14@naver.com>
2021-02-01 14:34:41 +0900

    feat : 문의하기 로그인 처리 추가.

--- src/projectB/model/question/QuestionController.java
+++ src/projectB/model/question/QuestionController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import projectB.model.login.LoginUtils;
 import projectB.model.questionService.QuestionService;
 
 @Controller
@@ -23,27 +24,19 @@ public class QuestionController {
 	
 	//==========================올리기================================
 	@RequestMapping("upload.aa")
-    public String upload(QuestionDTO dto, Model model,HttpSession session) throws Exception {
+    public String upload_question(QuestionDTO dto, Model model,HttpSession session) throws Exception {
+		System.out.println("upload_question run");
         
-        //임시 세션 아이디 입력
-        session.setAttribute("memId", "홍우찬테스트");
-        
-        String id = (String) session.getAttribute("memId");
-        System.out.println("session id:"+id);
-        
-        if(id != null) {
-            
-            List category = null;
-            System.out.println("wooch uploadForm run");
-            
-            model.addAttribute("dto", dto);
-            dto.setWriter((String)session.getAttribute("memId"));
-            System.out.println("Writer:"+dto.getWriter());
-            category = questionDAO.getCategory();
-            model.addAttribute("category", category);
-            System.out.println("category size:"+category.size()+"\n"+category);
-        }   
-        
+		String id = LoginUtils.getLoginID(session);
+		System.out.println("session id:" + id);
+
+		List category = null;
+
+		model.addAttribute("dto", dto);
+		dto.setWriter((String) session.getAttribute("memId"));
+		category = questionDAO.getCategory();
+		model.addAttribute("category", category);
+		
         return "question/questionUploadForm";
     }
 	 
