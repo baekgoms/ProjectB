05c80c67c69a327a82ccdb97801c61d689b7d07f
baekgoms <asx14@naver.com>
2021-02-04 17:57:14 +0900

    test

--- src/projectB/test/baek/baekTest.java
+++ src/projectB/test/baek/baekTest.java
@@ -1,17 +1,30 @@
 package projectB.test.baek;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import projectB.model.petitionService.PetitionService;
 import projectB.model.petitioner.PetitionerDTO;
 import projectB.model.petitionerService.PetitionerService;
 
 @Controller
 @RequestMapping("beakTest")
 public class baekTest {
+	@Autowired
+    private PetitionService petitionDAO = null;
+	
+	@RequestMapping("test.aa")
+	public String test() {
+		
+		return "test/Test";
+	}
 //	private static final int NOT_CONFIRM = 0;
 //	private static final int CONFIRM = 1;
 //	
